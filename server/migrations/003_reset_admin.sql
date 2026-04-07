a43daaa94c1c45a3e7c7399b04c35ccb7a6e8c14
realtape <entrepoker@gmail.com>
2026-04-07 17:02:41 -0400

    Migration: clean reset of admin org for re-registration

--- server/migrations/003_reset_admin.sql
+++ server/migrations/003_reset_admin.sql
@@ -1,4 +1,21 @@
--- Reset admin password for entrepoker@gmail.com to NWMHS2026
-UPDATE users
-SET password_hash = '$2a$12$3.9cvlkUn7fumaQ26wLrEudPfgXEvDJyREgUO//3xH8R5wGqEpITm'
-WHERE email = 'entrepoker@gmail.com';
+-- Reset admin: delete existing org/user so registration can be redone cleanly
+-- Safe to run multiple times (idempotent)
+DO $$
+DECLARE
+  v_org_id UUID;
+BEGIN
+  SELECT o.id INTO v_org_id
+  FROM users u
+  JOIN organizations o ON o.id = u.org_id
+  WHERE u.email = 'entrepoker@gmail.com'
+  LIMIT 1;
+
+  IF v_org_id IS NOT NULL THEN
+    DELETE FROM activities   WHERE org_id = v_org_id;
+    DELETE FROM tasks        WHERE org_id = v_org_id;
+    DELETE FROM deals        WHERE org_id = v_org_id;
+    DELETE FROM contacts     WHERE org_id = v_org_id;
+    DELETE FROM users        WHERE org_id = v_org_id;
+    DELETE FROM organizations WHERE id = v_org_id;
+  END IF;
+END $$;
